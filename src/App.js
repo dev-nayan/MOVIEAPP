62c07151ac138b10a086e1e2c39c857a5f1e352d
nayan jha <101447067+dev-nayan@users.noreply.github.com>
2022-09-09 07:06:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+
+import Banner from "./components/Banner";
+import Favourites from "./components/Favourites";
+import MovieList from "./components/MovieList";
+import Navbar from "./components/Navbar";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner /> 
+              <MovieList />
+            </>
+          }
+        />
+
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
